d2a5ed6fa6399a4b612bf8154cb871aada3dfc88
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-12-02 11:30:36 -0300

    Adicionando depth = 1 para melhor informações

--- core/serializers/categoria.py
+++ core/serializers/categoria.py
@@ -7,3 +7,4 @@ class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+        depth = 1
--- core/serializers/user.py
+++ core/serializers/user.py
@@ -7,6 +7,7 @@ class TrabalhadorPequeno(ModelSerializer):
     class Meta:
         model = User
         fields = ("foto", "name", "categoria", "tipo")
+        depth = 1
 
 
 class UserSerializer(ModelSerializer):
@@ -15,9 +16,11 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        depth = 1
 
 
 class PerfilSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("name", "foto", "capa", "publicacao", "publicacaoFoto", "descricao")
+        depth = 1
